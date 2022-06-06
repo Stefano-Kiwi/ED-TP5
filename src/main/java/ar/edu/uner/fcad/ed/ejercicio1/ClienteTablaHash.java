ce09601d0f7ca4279125384dc3ae0bf6f62dd780
David Zarcó <davidzarcofca@gmail.com>
2022-06-06 20:07:03 -0300

    main

--- src/main/java/ar/edu/uner/fcad/ed/ejercicio1/ClienteTablaHash.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio1/ClienteTablaHash.java
@@ -1,5 +1,8 @@
 package ar.edu.uner.fcad.ed.ejercicio1;
 
 public class ClienteTablaHash {
+    public static void main(String[] args) {
+        
+    }
     
 }
--- src/main/java/ar/edu/uner/fcad/ed/ejercicio3/ClienteArbolABBExt.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio3/ClienteArbolABBExt.java
@@ -1,5 +1,8 @@
 package ar.edu.uner.fcad.ed.ejercicio3;
 
 public class ClienteArbolABBExt {
+    public static void main(String[] args) {
+        
+    }
     
 }
