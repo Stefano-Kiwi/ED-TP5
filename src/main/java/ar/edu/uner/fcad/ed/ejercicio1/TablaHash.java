1c9f43cbba22d4dc8441aa097d6014cbb5f104b8
Stefano-Kiwi <stefanobudon@gmail.com>
2022-06-08 21:18:18 -0300

    agrego metodos tabla hash

--- src/main/java/ar/edu/uner/fcad/ed/ejercicio1/TablaHash.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio1/TablaHash.java
@@ -1,5 +1,53 @@
 package ar.edu.uner.fcad.ed.ejercicio1;
 
-public class TablaHash {
+import java.lang.reflect.Array;
+
+public class TablaHash<K,T> implements TablaHashInterfaz<K, T>{
+    
+    protected T [] tabla = null;     //  debe ir de tipo T
+    protected int cantElem = 0;
+
+    public TablaHash(Class clase,int tam) {
+        tabla = (T[]) Array.newInstance(clase, tam);
+    }
+    
+    
+    @Override
+    public void put(K clave, T valor) {
+
+        
+    }
+
+    @Override
+    public void remove(K clave) {
+
+
+    }
+
+    @Override
+    public T get(K clave) {
+
+        return null;
+    }
+
+    @Override
+    public boolean contains(K clave) {
+        return false;
+    }
+
+    @Override
+    public void makeEmpty() {
+        
+    }
+
+    @Override
+    public int size() {
+        return this.cantElem;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
     
 }
