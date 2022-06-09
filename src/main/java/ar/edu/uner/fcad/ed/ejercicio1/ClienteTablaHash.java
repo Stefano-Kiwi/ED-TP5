3bb683d6d72fa2939dff84049f5beae702b734a0
Stefano-Kiwi <stefanobudon@gmail.com>
2022-06-08 22:00:03 -0300

    Mejora en tabla hash

--- src/main/java/ar/edu/uner/fcad/ed/ejercicio1/ClienteTablaHash.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio1/ClienteTablaHash.java
@@ -2,7 +2,9 @@ package ar.edu.uner.fcad.ed.ejercicio1;
 
 public class ClienteTablaHash {
     public static void main(String[] args) {
-        
+        TablaHash tabla = new TablaHash(Integer.class, 40);
+        tabla.put(100, 40);
+        tabla.put(78, 40);
     }
     
 }
--- src/main/java/ar/edu/uner/fcad/ed/ejercicio1/TablaHash.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio1/TablaHash.java
@@ -6,7 +6,7 @@ public class TablaHash<K,T> implements TablaHashInterfaz<K, T>{
     
     protected T [] tabla = null;     //  debe ir de tipo T
     protected int cantElem = 0;
-
+    
     public TablaHash(Class clase,int tam) {
         tabla = (T[]) Array.newInstance(clase, tam);
     }
@@ -14,14 +14,22 @@ public class TablaHash<K,T> implements TablaHashInterfaz<K, T>{
     
     @Override
     public void put(K clave, T valor) {
-
+        int claveHash = clave.hashCode()%tabla.length;
+        System.out.println("El nuevo hashcode es: "+claveHash);
         
     }
-
+    
+    public int pruebaLineal(int clave){
+        
+        
+        
+        return 0;
+    }
+    
     @Override
     public void remove(K clave) {
-
-
+        
+        
     }
 
     @Override
