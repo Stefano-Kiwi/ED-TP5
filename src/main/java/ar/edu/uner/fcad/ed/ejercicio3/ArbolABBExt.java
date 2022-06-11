ffc851990d5ff3421c654fbb7ef1fc2e645a2366
DANIEL <DANIEL@DESKTOP-FOJGATN.local>
2022-06-11 18:54:34 -0300

    Avances puntos 3.

--- src/main/java/ar/edu/uner/fcad/ed/ejercicio3/ArbolABBExt.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio3/ArbolABBExt.java
@@ -1,34 +1,93 @@
 package ar.edu.uner.fcad.ed.ejercicio3;
+import ar.edu.uner.fcad.ed.arbolesabbyavl.ArbolABB;
+import ar.edu.uner.fcad.ed.arbolesabbyavl.NodoABB;
+import ar.edu.uner.fcad.ed.arbolesbinariosyheaps.NodoArbolBinario;
+import ar.edu.uner.fcad.ed.edlineales.ListaEnlazadaNoOrdenada;
 
 /**
  *
- * @author stefa
+ * @author David Zarcó
+ * @param <T>
  */
-public class ArbolABBExt <T extends Comparable<T>> implements ArbolABBExtInterfaz<T> {
+public class ArbolABBExt<T extends Comparable<T>> extends ArbolABB<T> implements ArbolABBExtInterfaz<T> {
 
     @Override
     public T min() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        NodoABB<T> nodoActual = raiz;
+
+        if (!raiz.tieneHijoIzquierdo()) {
+            return raiz.getValor();
+        } else {
+            while (nodoActual.tieneHijoIzquierdo() != false) {
+                nodoActual = nodoActual.getHijoIzquierdo();
+            }
+        }
+        return nodoActual.getValor();
     }
 
     @Override
     public T max() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
+        NodoABB<T> nodoActual = raiz;
+        
+
+        if (!raiz.tieneHijoDerecho()) {
+            return raiz.getValor();
+        } else {
+            while (nodoActual.tieneHijoDerecho() != false) {
+                nodoActual = nodoActual.getHijoDerecho();
+            }
+        }
+        return nodoActual.getValor();
     }
 
     @Override
     public T floor(T valor) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        NodoABB<T> nodoActual = raiz;
+        NodoABB<T> prox = raiz;
+
+        while (nodoActual.tieneHijoIzquierdo() || nodoActual.tieneHijoDerecho()) {
+
+            if  (valor.compareTo(nodoActual.getValor()) > 0) {
+                nodoActual = nodoActual.getHijoDerecho();
+            } else {
+                nodoActual = nodoActual.getHijoIzquierdo();
+            }
+            
+            
+            if (nodoActual.getValor().compareTo(valor) > 0) {
+                return nodoActual.getValor();
+            }
+
+        }
+        return nodoActual.getValor();
     }
 
     @Override
     public T ceiling(T valor) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        NodoABB<T> nodoActual = raiz;
+
+        while (nodoActual.tieneHijoIzquierdo() || nodoActual.tieneHijoDerecho()) {
+
+            if  (valor.compareTo(nodoActual.getValor()) < 0) {
+                nodoActual = nodoActual.getHijoIzquierdo();
+            } else {
+                nodoActual = nodoActual.getHijoDerecho();
+            }
+            if (nodoActual.getValor().compareTo(valor) > 0) {
+                return nodoActual.getValor();
+            }
+
+        }
+        return nodoActual.getValor();
     }
 
     @Override
     public boolean esPerfecto() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
--- src/main/java/ar/edu/uner/fcad/ed/ejercicio3/ClienteArbolABBExt.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio3/ClienteArbolABBExt.java
@@ -1,8 +1,36 @@
 package ar.edu.uner.fcad.ed.ejercicio3;
+import ar.edu.uner.fcad.ed.arbolesabbyavl.ArbolABB;
 
+/**
+ *
+ * @author David Zarcó
+ */
 public class ClienteArbolABBExt {
+    
     public static void main(String[] args) {
         
-    }
     
+      ArbolABBExt<Integer> tree = new ArbolABBExt<>();
+      
+      tree.add(10);
+      tree.add(15);
+      tree.add(20);
+      tree.add(3);
+      tree.add(6);
+      tree.add(9);
+      tree.add(12);
+      tree.add(4);
+      tree.add(13);
+      tree.add(16);
+      tree.add(2);
+      tree.add(21);
+      tree.add(31);
+      tree.add(88);
+      tree.add(26);
+      
+        System.out.println("Nodo con menor valor de clave: " + tree.min());
+        System.out.println("Nodo con mayor valor de clave: " + tree.max());
+        System.out.println("Valor menor al parametro 11: " + tree.floor(4));
+        System.out.println("Valor mayor al parametro 16: " + tree.ceiling(15));  
+    }
 }
